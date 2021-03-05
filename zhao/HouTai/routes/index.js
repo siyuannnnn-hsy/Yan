4aa9b0cd44b7a8091f68b8c4d5ba6233a59f1a28
zhaojingyu00 <2819361152@qq.com>
2021-03-05 17:11:56 +0800

    a

--- zhao/HouTai/routes/index.js
+++ zhao/HouTai/routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+//后台登录界面基础界面
 router.get('/', function(req, res, next) {
   res.render('takeIn', { title: 'Welcome' });
 });
+
+//后台注册界面基础界面
+router.get ('/regist',function(req,res){
+  res.render('regist',{title:'后台注册界面'})
+})
 
 module.exports = router;
