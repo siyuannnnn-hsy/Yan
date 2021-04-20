591e0be385c681e2e009fcc827462f03081df94d
zhaojingyu00 <2819361152@qq.com>
2021-04-20 17:45:07 +0800

    tiku

--- hsy/myapp/src/Container/Study/Study.js
+++ hsy/myapp/src/Container/Study/Study.js
@@ -39,29 +39,61 @@ export default class Study extends Component {
         </div>
         <div style={{ display: 'flex',height:'300px',color:'gray', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
             <div>
-              accord 使符合，相一致(with)
-            </div>    
-            <div>
-              account 记述，叙述
-            </div>
-            <div>
-              accumulate 积累，积攒，积聚
-            </div>
-            <div>
-              accurate 准确的 ，精确的 
-            </div>
-            <div>
-              accuse 指控，指责
-            </div>
-            <div>
-              accustom 使习惯于
-            </div>
-            <div>
-              acquaint 使认识，使了解
+              <p>accord 使符合，相一致(with)</p>accord 使符合，相一致(with)
+            {/* </div>    
+            <div> */}
+            <p style={{backgroundColor:'#fff'}}>account 记述，叙述</p>
+            <p style={{backgroundColor:'#fff'}}>accumulate 积累，积攒，积聚</p>
+            <p style={{backgroundColor:'#fff'}}>accurate 准确的 ，精确的 </p>
+            <p style={{backgroundColor:'#fff'}}> accuse 指控，指责</p>
+            <p style={{backgroundColor:'#fff'}}>accustom 使习惯于</p>
+            <p style={{backgroundColor:'#fff'}}>acquaint 使认识，使了解</p>
+            <p style={{backgroundColor:'#fff'}}>account 记述，叙述</p>
+            <p style={{backgroundColor:'#fff'}}>accumulate 积累，积攒，积聚</p>
+            {/* </div>
+            <div> */}
+              
+            {/* </div>
+            <div> */}
+              
+            {/* </div>
+            <div> */}
+             
+            {/* </div>
+            <div> */}
+              
+            {/* </div>
+            <div> */}
+              
             </div>
         </div>
         <div style={{ display: 'flex',height:'300px',color:'gray', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
-          研题库
+        <div>
+        <p>
+          1.为解决计算机与打印机之间速度不匹配的问题，通常设置一
+          个打印数据缓冲区，主机将要输出的数据依次写入该缓冲区，而打印机则
+          依次从该缓冲区中取出数据。该缓冲区的逻辑结构应该是
+          </p>
+          <p>
+            A.栈 B.队列 C.树 D.图</p>   
+           <p>
+             2.设栈S和队列Q的初始状态均为空，元素abcdefg依
+             次进入栈S。若每个元素出栈后立即进入队列Q，且7个元素出
+             队的顺序是bdcfeag，则栈S的容量至少是
+             </p>
+          <p>
+              A.1 B.2 C.3 D.4
+             </p>
+          <p>
+          给定二叉树图所示。设N代表二叉树的根，
+          L代表根结点的左子树，R代表根结点的右子树。若遍历后的结点
+          序列为3，1，7，5，6，2，4，则其遍历方式是
+          </p>
+          <p>
+           A.LRN B.NRL C.RLN D.RNL
+          </p>
+        </div>
+                 
         </div>
         <div style={{ display: 'flex',height:'300px',color:'gray', alignItems: 'center', justifyContent: 'center',  backgroundColor: '#fff' }}>
           学习期刊
