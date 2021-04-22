313ee0aa7490f30b26d775c1e6e10ddf10fd46a9
zhaojingyu00 <2819361152@qq.com>
2021-04-22 12:44:06 +0800

    a

--- hsy/myapp/src/Container/Start/takeIn.js
+++ hsy/myapp/src/Container/Start/takeIn.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { NavBar, Icon,WingBlank, Button} from 'antd-mobile';
 import {Link,Route} from 'react-router-dom'
 // import AppPetXinxi from './AppPetXinxi';
-// var code='PCAE';
+var code='PCAE';
 
 export default class AppZhuce extends Component {
     constructor(){
@@ -14,6 +14,50 @@ export default class AppZhuce extends Component {
         }
     }
    
+     /* 随机验证码 */
+     //声明一个变量用于存储生成的验证码
+     
+     changeImg=()=>{
+        //alert("换图片");
+        var arrays=new Array(
+            '1','2','3','4','5','6','7','8','9','0',
+            'a','b','c','d','e','f','g','h','i','j',
+            'k','l','m','n','o','p','q','r','s','t',
+            'u','v','w','x','y','z',
+            'A','B','C','D','E','F','G','H','I','J',
+            'K','L','M','N','O','P','Q','R','S','T',
+            'U','V','W','X','Y','Z'				
+        );
+        code='';//重新初始化验证码
+        //alert(arrays.length);
+        //随机从数组中获取四个元素组成验证码
+        for(var i=0;i<4;i++){
+        //随机获取一个数组的下标
+            var r=parseInt(Math.random()*arrays.length);
+            code+=arrays[r];
+            //alert(arrays[r]);
+        }
+        //alert(code);
+        document.getElementById('code').innerHTML=code;//将验证码写入指定区域
+    }		
+    
+    //效验验证码(表单被提交时触发)
+    check=()=>{
+        //获取用户输入的验证码
+        var input_code=document.getElementById('vcode').value;
+        //alert(input_code+"----"+code);
+        if(input_code.toLowerCase()==code.toLowerCase())
+        {
+            //验证码正确(表单提交)
+            this.register();
+            window.location='/login';
+            return true;
+        }
+        alert("请输入正确的验证码!");
+        
+        //验证码不正确,表单不允许提交
+        return false;
+    }
  
     render() {
         
@@ -31,16 +75,15 @@ export default class AppZhuce extends Component {
                      style={{height:'100px',width:'100px',marginTop:'25px',marginLeft:'-172px'}} />
                     <i style={{fontSize:22,color:'balck'}} className='iconfont icon-back'></i>
                     <div style={{color:'black',fontSize:'20px',paddingLeft:"-150px",paddingTop:'20px',marginLeft:'-150px'}}>
-                        注册成为考研人
+                        登录
                     </div>
                 </div>
                 <div style={{marginTop:'80px'}}>
-                    <div className='userName' style={{color:'black',marginTop:'28px',marginLeft:'30px',fontSize:'20px'}}>
+                    {/* <div className='userName' style={{color:'black',marginTop:'28px',marginLeft:'30px',fontSize:'20px'}}>
                         昵称
                         <input style={{marginLeft:'18px',width:'248px'}} value='小黄'/>
-                            {/* 小黄 */}
-                            {/* </input> */}
-                    </div>
+                            
+                    </div> */}
                     <div className='userName' style={{color:'black',marginTop:'28px',marginLeft:'30px',fontSize:'20px'}}>
                         手机号
                         <input style={{marginLeft:'18px',width:'228px'}} value='12563489523'/>
@@ -53,9 +96,54 @@ export default class AppZhuce extends Component {
                             {/* 123456 */}
                             {/* </input> */}
                     </div>
-                    <div style={{color:'black',float:'left',marginTop:'20px',fontSize:'20px',marginLeft:'22px'}}>我是</div>
+                    <WingBlank>
+                {/* height: '250px',background:'#eee' */}
+                <div style={{}} >
+                    {/* <div style={{width:'100%',margin:'0 auto',lineHeight:3,marginTop:20,position:"relative"}}>
+                        <i style={{fontSize:22,color:'rgb(29,174,169)'}} className='iconfont icon-gerenxinxi'></i>
+                        <input onChange={this.nameChange} style={{width:'90%',lineHeight:3,marginLeft:10,borderRadius:'10px',paddingLeft:5,position:"absolute",top:10,border:'1px solid rgb(29,174,169)'}} type='text' id='username' name='username' placeholder='用户名' />
+                    </div>
+                    <div style={{width:'100%',margin:'0 auto',lineHeight:3,marginTop:20,position:"relative"}}>
+                        <label><i style={{fontSize:22,color:'rgb(29,174,169)'}} className='iconfont icon-wode'></i></label>
+                        <input onChange={this.telChange} style={{width:'90%',lineHeight:3,marginLeft:10,borderRadius:'10px',paddingLeft:5,position:"absolute",top:10,border:'1px solid rgb(29,174,169)'}} type='text' id='tel' name='tel' placeholder='手机号' />
+                    </div>
+                    <div style={{width:'100%',margin:'0 auto',lineHeight:3,marginTop:20,position:"relative"}}>
+                        <label><i style={{fontSize:22,color:'rgb(29,174,169)'}} className='iconfont icon-chongwuleyuan'></i></label>
+                        <input onChange={this.pwdChange} style={{width:'90%',lineHeight:3,marginLeft:10,borderRadius:'10px',paddingLeft:5,position:"absolute",top:10,border:'1px solid rgb(29,174,169)'}} type='password'  id='pwd' name='pwd' placeholder='密码' />
+                    </div> */}
+                    <div style={{width:'100%',margin:'0 auto',lineHeight:3,marginTop:20,position:"relative"}}>
+                        <label>
+                            <i style={{fontSize:22,color:'rgb(29,174,169)'}} className='iconfont icon-yanjing'></i>
+                            </label>
+                        <input style={{width:'50%',lineHeight:3,marginLeft:10,borderRadius:'10px',
+                        paddingLeft:5,position:"absolute",top:10,border:'1px solid rgb(29,174,169)'}} 
+                        type="text" id="vcode" placeholder="验证码"    />
+                        <button id="vvcode"  
+                        title="看不清，换一张" 
+                        onClick={this.changeImg} 
+                        style={{background:'#1daea9',width:'30%',float:'right',lineHeight:3,borderRadius:'10px',marginTop:8}}>
+                            <span id="code" title="看不清，换一张">获取验证码</span>
+                            </button>
+                    </div>
+                    {/* <Link to='/petxinxi'> */}
+                        {/* <input type='submit' style={{width:'100%',height:50,margin:'0 auto',background:'#1daea9',color:'#fff',borderRadius:'40px',fontSize:25,marginTop:20}} value='注册'  onClick={this.check}/> */}
+                        {/* </Link> */}
+                   
+                    
+                </div>
+                </WingBlank>
+                    {/* <div className='userName' style={{color:'black',marginTop:'28px',marginLeft:'30px',fontSize:'20px'}}>
+                        验证码
+                        <input type="text" id="vcode" placeholder="验证码" style={{width:'80px'}}/>
+                        <span id="code" title="看不清，换一张"></span>
+                        <span id="vvcode"  title="看不清，换一张" onclick="changeImg()">换一张</span> */}
+                        {/* <input style={{marginLeft:'18px',width:'248px'}} value='123456'/> */}
+                            {/* 123456 */}
+                            {/* </input> */}
+                    {/* </div> */}
+                    {/* <div style={{color:'black',float:'left',marginTop:'20px',fontSize:'20px',marginLeft:'22px'}}>我是</div>
                     <input style={{marginLeft:'18px',width:'80px',float:'left',marginTop:'20px',fontSize:'20px'}} value='123456'/>
-                    <div style={{color:'black',float:'left',marginTop:'20px',fontSize:'20px',marginLeft:'20px'}}>届考生</div>
+                    <div style={{color:'black',float:'left',marginTop:'20px',fontSize:'20px',marginLeft:'20px'}}>届考生</div> */}
                     {/* <div className='userName' style={{color:'black',marginTop:'28px',marginLeft:'30px',fontSize:'20px'}}>
                         座右铭
                         <input style={{marginLeft:'18px',width:'228px',fontSize:'15px'}} value='这个人很懒，什么也没有留下'/>
@@ -72,7 +160,7 @@ export default class AppZhuce extends Component {
                            
                     </div> */}
                     <Link to={'/'}>
-                    <Button style={{marginTop:'80px',backgroundColor:'gray'}}>登录</Button>
+                    <Button style={{marginTop:'280px',backgroundColor:'gray'}}>登录</Button>
                     </Link>
                     
                     </div>
